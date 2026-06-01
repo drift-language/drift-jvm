86048d517c21ca4352e51a765fdb3f655e56d2da
belicfr <96206878+belicfr@users.noreply.github.com>
2026-06-01 22:32:36 +0200

    feat: create helpers for JVM typing from HIR

---
+++ src/main/kotlin/drift/jvm/emitters/types/helpers/PrimitiveType.kt
@@ -0,0 +1,70 @@
+/*
+ * Drift Programming Language
+ * Drift JVM Backend
+ *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)
+ *
+ * This source code is licensed under the MIT License.
+ * See the LICENSE file in the root directory for details.
+ */
+
+package drift.jvm.emitters.types.helpers
+
+import drift.hir.PrimitiveKind
+
+
+const val stringAsmType = "Ljava/lang/String;"
+const val objectAsmType = "Ljava/lang/Object;"
+
+
+/**
+ * Primitive types conversion between HIR and JVM.
+ *
+ * @author Jonathan (GitHub: belicfr)
+ */
+enum class PrimitiveType(
+    val hirType: PrimitiveKind?,
+    val asmType: String) {
+
+    VOID(PrimitiveKind.VOID, "V"),
+
+    INT(PrimitiveKind.INT, "I"),
+    UINT(PrimitiveKind.UINT, "I"),
+
+    LONG(PrimitiveKind.INT64, "L"),
+
+    BOOLEAN(PrimitiveKind.BOOL, "Z"),
+
+    STRING(PrimitiveKind.STRING, stringAsmType),
+
+    ANY(null, objectAsmType),
+
+    NULL(PrimitiveKind.NULL, objectAsmType)
+    ;
+
+
+    companion object {
+
+        fun fromHirType(hirType: PrimitiveKind) : PrimitiveType =
+            when (hirType) {
+                PrimitiveKind.VOID      -> VOID
+                PrimitiveKind.INT       -> INT
+                PrimitiveKind.UINT      -> UINT
+                PrimitiveKind.INT64     -> LONG
+                PrimitiveKind.BOOL      -> BOOLEAN
+                PrimitiveKind.STRING    -> STRING
+                PrimitiveKind.NULL      -> NULL
+            }
+
+        fun fromAsmType(asmType: String) : PrimitiveType =
+            when (asmType) {
+                "V"             -> VOID
+                "I"             -> INT
+                "Z"             -> BOOLEAN
+                stringAsmType   -> STRING
+                objectAsmType   -> error("'$objectAsmType' is ambiguous to be translated to a HIR primitive type.")
+
+                else -> error("Unknown asm type: $asmType")
+            }
+    }
+}
---
+++ src/main/kotlin/drift/jvm/emitters/types/helpers/TypeConverter.kt
@@ -0,0 +1,37 @@
+/*
+ * Drift Programming Language
+ * Drift JVM Backend
+ *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)
+ *
+ * This source code is licensed under the MIT License.
+ * See the LICENSE file in the root directory for details.
+ */
+
+package drift.jvm.emitters.types.helpers
+
+import drift.hir.HIRPrimitiveType
+import drift.hir.HIRType
+
+object TypeConverter {
+
+    fun toAsmType(hirType: HIRType) : String {
+        return if (hirType is HIRPrimitiveType) {
+            PrimitiveType
+                .fromHirType(hirType.kind)
+                .asmType
+        } else {
+            PrimitiveType
+                .ANY
+                .asmType
+        }
+    }
+
+    fun formatTypes(input: List<HIRType>, output: HIRType) : String {
+        val formattedInputTypes = input
+            .joinToString(transform = this::toAsmType)
+        val formattedOutputType = toAsmType(output)
+
+        return "($formattedInputTypes)$formattedOutputType"
+    }
+}
