e90832551e14fb088876bbd07081d5010cbc5ee9
belicfr <96206878+belicfr@users.noreply.github.com>
2026-06-02 14:35:27 +0200

    feat: create ReturnEmitter (stmt)

---
+++ src/main/kotlin/drift/jvm/emitters/statements/ReturnEmitter.kt
@@ -0,0 +1,69 @@
+/*
+ * Drift Programming Language
+ * Drift JVM Backend
+ *
+ * Copyright (c) 2026. Jonathan (GitHub: belicfr)
+ *
+ * This source code is licensed under the MIT License.
+ * See the LICENSE file in the root directory for details.
+ */
+
+package drift.jvm.emitters.statements
+
+import drift.hir.*
+import drift.jvm.emitters.EmitContext
+import drift.jvm.emitters.InnerEmitter
+import drift.jvm.emitters.expressions.ExpressionEmitter
+import org.objectweb.asm.Opcodes.*
+
+
+/**
+ *
+ * 
+ * @author Jonathan (GitHub: belicfr)
+ */
+class ReturnEmitter(
+    private val context: EmitContext)
+    : InnerEmitter<HIRReturn> {
+
+    override fun emit(node: HIRReturn) {
+        if (node.value == null) {
+            context.methodVisitor.visitInsn(RETURN)
+            // NOTE: In that case, we are handling a VOID
+            //  special value (expr = null).
+            //  We deduct that the value is VOID by logic,
+            //  we finally visit instruction with RETURN (void)
+            //  opcode.
+
+            return
+        }
+
+        val value = node.value!!
+
+        ExpressionEmitter(context)
+            .emit(value)
+
+        val returnOpcode = when (val type = value.type) {
+            is HIRPrimitiveType -> getReturnOpcodeFromPrimitive(type.kind)
+            is HIRClassType,
+            is HIROptionalType,
+            is HIRUnionType     -> ARETURN
+
+            else                -> error("Unexpected type")
+        }
+
+        context.methodVisitor.visitInsn(returnOpcode)
+    }
+
+    private fun getReturnOpcodeFromPrimitive(primitiveKind: PrimitiveKind) =
+        when (primitiveKind) {
+            PrimitiveKind.BOOL,
+            PrimitiveKind.INT,
+            PrimitiveKind.UINT      -> IRETURN
+            PrimitiveKind.INT64     -> LRETURN
+            PrimitiveKind.STRING,
+            PrimitiveKind.NULL      -> ARETURN
+
+            else                    -> error("Unexpected primitive")
+        }
+}
