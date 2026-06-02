c4f872376214a0d865a33de9620aa1c440280575
belicfr <96206878+belicfr@users.noreply.github.com>
2026-06-02 14:35:15 +0200

    refactor: implement context and null cases

--- src/main/kotlin/drift/jvm/emitters/expressions/LiteralEmitter.kt
+++ src/main/kotlin/drift/jvm/emitters/expressions/LiteralEmitter.kt
@@ -10,13 +10,15 @@
 
 package drift.jvm.emitters.expressions
 
-import drift.hir.*
-import drift.jvm.emitters.Emitter
+import drift.hir.HIRLiteral
+import drift.hir.HIRPrimitiveType
+import drift.hir.PrimitiveKind
+import drift.jvm.emitters.EmitContext
+import drift.jvm.emitters.InnerEmitter
 import drift.jvm.emitters.opcodes.OpcodesPlus.BIPUSH_MAX
 import drift.jvm.emitters.opcodes.OpcodesPlus.BIPUSH_MIN
 import drift.jvm.emitters.opcodes.OpcodesPlus.SIPUSH_MAX
 import drift.jvm.emitters.opcodes.OpcodesPlus.SIPUSH_MIN
-import org.objectweb.asm.MethodVisitor
 import org.objectweb.asm.Opcodes.*
 
 
@@ -26,7 +28,8 @@ import org.objectweb.asm.Opcodes.*
  * @author Jonathan (GitHub: belicfr)
  */
 class LiteralEmitter(
-    private val methodVisitor: MethodVisitor) : Emitter<HIRLiteral, Unit> {
+    private val context: EmitContext)
+    : InnerEmitter<HIRLiteral> {
 
     override fun emit(node: HIRLiteral) {
         when (node.type) {
@@ -37,51 +40,38 @@ class LiteralEmitter(
     }
 
     private fun emitPrimitive(node: HIRLiteral) {
+        if (node.value == null) {
+            context.methodVisitor.visitInsn(ACONST_NULL)
+
+            return
+        }
+
         when ((node.type as HIRPrimitiveType).kind) {
             PrimitiveKind.BOOL -> {
-                if (node.value == null)
-                    TODO("NULL behavior on BOOL literal emitting")
-
-                val value: Boolean = node.value as Boolean
                 val opcode =
-                    if (value) ICONST_1
+                    if (node.value as Boolean) ICONST_1
                     else ICONST_0
 
-                methodVisitor.visitInsn(opcode)
+                context.methodVisitor.visitInsn(opcode)
             }
-            PrimitiveKind.INT64 -> {
-                if (node.value == null)
-                    TODO("NULL behavior on INT64 literal emitting")
-
-                val value: Long = node.value as Long
-
-                methodVisitor.visitLdcInsn(value)
-            }
-            PrimitiveKind.INT, PrimitiveKind.UINT -> {
-                if (node.value == null)
-                    TODO("NULL behavior on INT literal emitting")
-
+            PrimitiveKind.INT64 ->
+                context.methodVisitor.visitLdcInsn(node.value as Long)
+            PrimitiveKind.INT, PrimitiveKind.UINT ->
                 when (val value: Int = node.value as Int) {
                     in ICONST_M1..ICONST_5 ->
-                        methodVisitor.visitInsn(ICONST_0 + value)
+                        context.methodVisitor.visitInsn(ICONST_0 + value)
                     in BIPUSH_MIN..BIPUSH_MAX ->
-                        methodVisitor.visitIntInsn(BIPUSH, value)
+                        context.methodVisitor.visitIntInsn(BIPUSH, value)
                     in SIPUSH_MIN..SIPUSH_MAX ->
-                        methodVisitor.visitIntInsn(SIPUSH, value)
+                        context.methodVisitor.visitIntInsn(SIPUSH, value)
 
-                    else -> methodVisitor.visitLdcInsn(value)
+                    else ->
+                        context.methodVisitor.visitLdcInsn(value)
                 }
-            }
-            PrimitiveKind.STRING -> {
-                if (node.value == null)
-                    TODO("NULL behavior on STRING literal emitting")
-
-                val value: String = node.value as String
-
-                methodVisitor.visitLdcInsn(value)
-            }
+            PrimitiveKind.STRING ->
+                context.methodVisitor.visitLdcInsn(node.value as String)
             PrimitiveKind.NULL ->
-                methodVisitor.visitInsn(ACONST_NULL)
+                context.methodVisitor.visitInsn(ACONST_NULL)
             PrimitiveKind.VOID ->
                 error("VOID cannot be a literal")
         }
